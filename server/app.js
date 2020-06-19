58d997d089637d1123ec8bc3cc62182f54aba2b0
joseCastro-76 <56211699+joseCastro-76@users.noreply.github.com>
2020-06-19 14:12:00 -0700

    Initial Upload. Adds / and /data route.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 // import files and packages up here
-
-
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data');
 // create your express server below
-var app;
+var app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'));
 
+app.get('/',(req, res) => res.status(200).type('html').send(req.body));
+app.get('/data', (req, res) => res.send(data));
 // finally export the express application
 module.exports = app;
